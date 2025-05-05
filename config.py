b3ee7ac0c1c6e3ad6160fa3d137d2cb95c37be57
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-06 01:26:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # Replace with your bot token
-MONGO_URI = "mongodb://localhost:27017/mychannelbot"  # Replace with your MongoDB URI
-SUPPORT_CHAT = "@MySupportChat"
-UPDATES_CHANNEL = "@MyUpdatesChannel"
-DEVELOPER = "@MyBotDeveloper"
+BOT_TOKEN = "5792496493:AAEdISosRZptqrYa-yNbO8R9zrat-L5TiIQ"  # Replace with your bot token
+MONGO_URI = "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB URI
+SUPPORT_CHAT = "@joinnowearn"
+UPDATES_CHANNEL = "@joinnowearn
+DEVELOPER = "@akash5213"
